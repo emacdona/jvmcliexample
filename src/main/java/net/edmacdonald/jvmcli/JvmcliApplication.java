b13ccec3cb6611e872b4bcfa2a5cf4ea40e21a1c
Ed MacDonald <edmacdonald555@gmail.com>
2025-05-05 19:15:17 -0400

    initial revision (thanks spring boot inializr)

---
+++ src/main/java/net/edmacdonald/jvmcli/JvmcliApplication.java
@@ -0,0 +1,13 @@
+package net.edmacdonald.jvmcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvmcliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvmcliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/edmacdonald/jvmcli/JvmcliApplicationTests.java
@@ -0,0 +1,13 @@
+package net.edmacdonald.jvmcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvmcliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
