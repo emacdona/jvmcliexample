96e64ca53047018e93bd7dc19ec914e06b702595
Ed MacDonald <edmacdonald555@gmail.com>
2025-05-06 12:53:31 +0000

    Scala/Java joint compilation... with a test class to prove it worked

--- src/main/java/net/edmacdonald/jvmcli/Command.java
+++ src/main/java/net/edmacdonald/jvmcli/Command.java
@@ -1,6 +1,7 @@
 package net.edmacdonald.jvmcli;
 
 import net.edmacdonald.jvmcli.command.Main;
+import net.edmacdonald.jvmcli.command.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -8,14 +9,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class Command implements CommandLineRunner {
     private final Main main;
+    private final Test test;
 
     @Autowired
-    public Command(Main main) {
+    public Command(Main main, Test test) {
         this.main = main;
+        this.test = test;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("It worked? %s".formatted(test.getString()));
         System.exit(main.getCommandLine().execute(args));
     }
 }
---
+++ src/main/scala/net/edmacdonald/jvmcli/command/Test.scala
@@ -0,0 +1,8 @@
+package net.edmacdonald.jvmcli.command
+
+import org.springframework.stereotype.Component
+
+@Component
+class Test {
+  def getString(): String = "Hello World"
+}
