e1579f219ef31600295bfb8b36f9dbee10e5ee21
Ed MacDonald <edmacdonald555@gmail.com>
2025-05-23 03:47:29 +0000

    It's working! Aspect based command history!! woo!

--- src/main/java/net/edmacdonald/jvmcli/JvmcliApplication.java
+++ src/main/java/net/edmacdonald/jvmcli/JvmcliApplication.java
@@ -1,19 +1,13 @@
 package net.edmacdonald.jvmcli;
 
 import net.edmacdonald.jvmcli.command.Main;
-import net.edmacdonald.jvmcli.entity.CommandArgument;
-import net.edmacdonald.jvmcli.entity.CommandInvocation;
 import net.edmacdonald.jvmcli.impldetails.runtimehints.LiquibaseRuntimeHints;
-import net.edmacdonald.jvmcli.repository.CommandInvocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.stereotype.Component;
-
-import java.math.BigInteger;
 
 @SpringBootApplication
 @ImportRuntimeHints(LiquibaseRuntimeHints.class)
@@ -26,28 +20,14 @@ public class JvmcliApplication {
     @Component
     public static class CliEntrypoint implements CommandLineRunner {
         private final Main main;
-        private final CommandInvocationRepository commandInvocationRepository;
-        private final String applicationName;
 
         @Autowired
-        public CliEntrypoint(Main main, CommandInvocationRepository commandInvocationRepository, @Value("${spring.application.name}") String applicationName) {
+        public CliEntrypoint(Main main) {
             this.main = main;
-            this.commandInvocationRepository = commandInvocationRepository;
-            this.applicationName = applicationName;
         }
 
         @Override
         public void run(String... args) throws Exception {
-            long order = 10;
-            CommandInvocation commandInvocation = new CommandInvocation();
-            commandInvocation.setCommand(applicationName);
-            for (String arg : args) {
-                CommandArgument commandArgument = new CommandArgument();
-                commandArgument.setArg(arg);
-                commandArgument.setArgumentOrder(BigInteger.valueOf(order));
-                commandInvocation.addArg(commandArgument);
-            }
-            commandInvocationRepository.save(commandInvocation);
             System.exit(main.getCommandLine().execute(args));
         }
     }
--- src/main/scala/net/edmacdonald/jvmcli/history/history.scala
+++ src/main/scala/net/edmacdonald/jvmcli/history/history.scala
@@ -1,35 +1,61 @@
 package net.edmacdonald.jvmcli.history
 
-import net.edmacdonald.jvmcli.history.History.getBean
+import net.edmacdonald.jvmcli.entity.{CommandArgument, CommandInvocation}
+import net.edmacdonald.jvmcli.history.HistoryAdvice.getBean
 import net.edmacdonald.jvmcli.repository.CommandInvocationRepository
+import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.{Aspect, Before, Pointcut}
+import org.springframework.beans.factory.annotation.{Autowired, Value}
 import org.springframework.context.{ApplicationContext, ApplicationContextAware}
 import org.springframework.stereotype.Component
 
+import java.math.BigInteger
+import scala.util.chaining.*
+
 @Aspect
 @Component
-class History extends ApplicationContextAware {
+class HistoryAdvice extends ApplicationContextAware {
 
-  @Pointcut("execution(public * picocli.CommandLine.execute(..))")
+  @Pointcut("execution(public int picocli.CommandLine.execute(String...))")
   private def commandExecution(): Unit = {}
 
-  @Before("net.edmacdonald.jvmcli.history.History.commandExecution()")
-  def log(): Unit = {
-    var commandInvocationRepository: CommandInvocationRepository =
-      getBean(classOf[CommandInvocationRepository])
-
-    println(s"EFM: Before command execution: ${commandInvocationRepository}")
+  @Before("net.edmacdonald.jvmcli.history.HistoryAdvice.commandExecution()")
+  def recordInvocation(joinPoint: JoinPoint): Unit = {
+    val service: HistoryService = getBean(classOf[HistoryService])
+    service.recordInvocation(
+      joinPoint.getArgs()(0)
+        .asInstanceOf[Array[String]]
+    )
   }
 
   override def setApplicationContext(ctx: ApplicationContext): Unit = {
-    History.context = ctx
+    HistoryAdvice.context = ctx
   }
 }
 
-object History {
-  @volatile private var context: ApplicationContext = _
+object HistoryAdvice {
+  @volatile private var context: ApplicationContext | Null = null
 
   def getBean[T](clazz: Class[T]): T = {
     context.getBean(clazz)
+  }
+}
+
+@Component
+class HistoryService @Autowired()(val commandInvocationRepository: CommandInvocationRepository,
+                                  @Value("${spring.application.name}") val applicationName: String) {
+  def recordInvocation(args: Array[String]): Unit = {
+    commandInvocationRepository.save(
+      new CommandInvocation().pipe { ci =>
+        ci.command = applicationName
+        args.foreach(arg =>
+          ci.addArg(new CommandArgument().pipe { ca =>
+            ca.arg = arg
+            ca.argumentOrder = BigInteger.ZERO
+            ca
+          }))
+        ci
+      }
+    )
   }
 }
