f6e9730476c45d3d73ad014acc0609ebc9f914c2
Ed MacDonald <edmacdonald555@gmail.com>
2025-09-21 18:25:54 -0400

    copilot says I should group by this as well

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -3,5 +3,5 @@ select  ci.COMMAND, array_agg(ca.ARG)
 from    COMMAND_INVOCATION ci
             inner join
         COMMAND_ARGUMENT ca on ca.COMMAND_INVOCATION_ID = ci.id
-group by ci.id;
+group by ci.id, ci.COMMAND;
 ;
