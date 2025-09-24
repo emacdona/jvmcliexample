6d3a756670f07b8d8746b5cd7733a8b01b811499
Ed MacDonald <edmacdonald555@gmail.com>
2025-09-24 16:28:30 -0400

    Holy shit, what an adventure. But... scala command working. looks like a few things going on here... - annotating the field doesn't seem to be enough. if i annotate the   setter, picocli sees it. if i annotate teh field, it does not (it   doesn't even show up in the "help" - if I annotate the setter, it's not reachable (as determined by Graal),   so it doesn't included it in the image. Despite it working for a Java   class. So, I have to declate it in reflect-config.json

--- src/main/scala/net/edmacdonald/jvmcli/command/commands.scala
+++ src/main/scala/net/edmacdonald/jvmcli/command/commands.scala
@@ -3,11 +3,23 @@ package net.edmacdonald.jvmcli.command
 import net.edmacdonald.jvmcli.command.example.Example
 import org.springframework.stereotype.Component
 import picocli.CommandLine
+import picocli.CommandLine.{Command, Parameters}
 
 import java.util.concurrent.Callable
+//import scala.annotation.meta.field
+//import scala.beans.BeanProperty
+import scala.compiletime.uninitialized
 
 @Component
-@CommandLine.Command(name = "find", mixinStandardHelpOptions = true, description = Array("Find files."))
-class Find extends Example.ExampleSubCommand, Callable[Integer] {
-  override def call(): Integer = 0
+@Command(name = "find", mixinStandardHelpOptions = true, description = Array("Find files."))
+class Find extends Example.ExampleSubCommand with Callable[Integer] {
+  //@(Parameters @field)(index = "0", description = Array("Starting directory."))
+  private var directory: String = "."
+
+  @Parameters(index = "0", description = Array("Starting directory."))
+  def setDirectory(d: String): Unit = {
+    directory = d
+  }
+
+  override def call(): Integer = return 0
 }
