82b22b6e153f1454bb68bd8ca41d45c9760793ac
Ed MacDonald <edmacdonald555@gmail.com>
2025-11-02 15:01:12 -0500

    System.exit(...) kills the jvm running the tests! So we now have a way for tests to pass even though our progam calls System.exit. We also have a framework in place that will allow for Graal metadata to be collected during tests... so that we don't have do manually create metadata files ourselves. This needs to be tested more, however.

--- src/main/java/net/edmacdonald/jvmcli/JvmcliApplication.java
+++ src/main/java/net/edmacdonald/jvmcli/JvmcliApplication.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.ExitCodeGenerator;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +15,25 @@ import org.springframework.stereotype.Component;
 @ImportRuntimeHints(LiquibaseRuntimeHints.class)
 public class JvmcliApplication {
     public static void main(String[] args) {
-        SpringApplication.run(JvmcliApplication.class, args);
+        System.exit(run(args));
     }
 
+    /**
+     * Starts the Spring context, runs the CLI, returns the exit code without exiting the JVM.
+     */
+    public static int run(String... args) {
+        ConfigurableApplicationContext ctx = SpringApplication.run(JvmcliApplication.class, args);
+        try {
+            return SpringApplication.exit(ctx);
+        } finally {
+            ctx.close();
+        }
+    }
 
     @Component
-    public static class CliEntrypoint implements CommandLineRunner {
+    public static class CliEntrypoint implements CommandLineRunner, ExitCodeGenerator {
         private final Main main;
+        private volatile int exitCode = 0;
 
         @Autowired
         public CliEntrypoint(Main main) {
@@ -27,8 +41,13 @@ public class JvmcliApplication {
         }
 
         @Override
-        public void run(String... args) throws Exception {
-            System.exit(main.getCommandLine().execute(args));
+        public void run(String... args) {
+            this.exitCode = main.getCommandLine().execute(args);
+        }
+
+        @Override
+        public int getExitCode() {
+            return exitCode;
         }
     }
 }
